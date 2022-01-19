90133c27b8d04963ccd71248170c6edfdfd16eb5
kirill0304 <35666066+kirill0304@users.noreply.github.com>
2022-01-19 14:01:09 +0200

    Update RadioButton.java

--- src/main/java/pagesloremipsum/RadioButton.java
+++ src/main/java/pagesloremipsum/RadioButton.java
@@ -5,14 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class RadioButton extends HomePage {
-    @FindBy(xpath  = "//input[@value='paragraphs']")
-    private WebElement paragraphsRadiobutton;
-    @FindBy(xpath  = "//input[@value='words']")
-    private WebElement wordsRadiobutton;
-    @FindBy(xpath  = "//input[@value='bytes']")
-    private WebElement bytesRadiobutton;
-    @FindBy(xpath  = "//input[@value='lists']")
-    private WebElement listsRadiobutton;
+   
 
     public RadioButton(WebDriver driver) {
         super(driver);
